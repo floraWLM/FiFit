01075776df423492c0ad5c3f8b9f7682f39892e4
Lemeng Wang <wang.15543@osu.edu>
2024-10-19 11:03:58 -0400

    Initial state

---
+++ Fifits/Fifits/FifitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FifitsApp.swift
+//  Fifits
+//
+//  Created by Lemeng Wang on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FifitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
