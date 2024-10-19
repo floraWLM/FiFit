843bd7f5aab4867144e9047fc5d4f47511381fcd
jianbo-zhao1 <zjianbo1@gmail.com>
2024-10-19 14:21:38 -0400

    Created WelcomeView which gathers users name and birthdate

---
+++ Fifits/Fifits/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Fifits
+//
+//  Created by Allen Zhao on 10/19/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ Fifits/Fifits/Views/WelcomeView.swift
@@ -0,0 +1,108 @@
+//
+//  WelcomeView.swift
+//  Fifits
+//
+//  Created by Allen Zhao on 10/19/24.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Spacer()
+                Text("Welcome to FiFits")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 40)
+                    .frame(maxHeight: .infinity, alignment: .center)
+                Spacer()
+                NavigationLink(destination: EnterNameView()) {
+                    Text("Get Started")
+                        .font(.headline)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+                .frame(maxHeight: .infinity, alignment: .bottom)
+            }
+            .padding()
+        }
+    }
+}
+
+struct EnterNameView: View {
+    @AppStorage("username") var userName: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("Enter Your Name")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+                .frame(maxHeight: .infinity, alignment: .center)
+            
+            TextField("Your Name", text: $userName)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom, 40)
+                .multilineTextAlignment(.center)
+            
+            NavigationLink(destination: EnterBirthDateView()) {
+                Spacer()
+                Text("Next")
+                    .font(.headline)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(userName.isEmpty ? Color.gray : Color.blue)
+                    .cornerRadius(10)
+            }
+            .disabled(userName.isEmpty)
+            .padding()
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .padding()
+    }
+}
+
+struct EnterBirthDateView: View {
+    @State private var bDate = Date()
+    
+    var body: some View {
+        VStack {
+            Text("Enter Your Birth Date")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+                .frame(maxHeight: .infinity, alignment: .center)
+            
+            DatePicker("Date",selection: $bDate, displayedComponents: .date)
+                .datePickerStyle(WheelDatePickerStyle())
+                .padding(.bottom, 40)
+            
+            Button(action: {
+            }) {
+                Text("Finish")
+                    .font(.headline)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(bDate > Date() ? Color.gray : Color.blue)
+                    .cornerRadius(10)
+            }
+            .disabled(bDate > Date())
+            .padding()
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
