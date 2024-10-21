17f0fcd389aebdb6f114c6e8d5d5c101a5e1495e
Allen <zjianbo1@gmail.com>
2024-10-21 12:56:07 -0400

    Update WeatherView.swift

--- Fifits/Fifits/Views/WeatherView.swift
+++ Fifits/Fifits/Views/WeatherView.swift
@@ -11,12 +11,12 @@ import CoreLocation
 
 struct WeatherView: View {
     @StateObject private var locationManager = LocationManager()
-    @Binding var weatherData: WeatherData?
+    @State private var weatherData: WeatherData?
     
     var body: some View {
         VStack{
             if let weatherData = weatherData {
-                Text("\(Int(weatherData.temperature))°F").font(.custom("", size: 70))
+                Text("\(Int(weatherData.temperature))°C").font(.custom("", size: 70))
                     .padding()
                 VStack{
                     Text("\(weatherData.locationName)")
@@ -29,7 +29,7 @@ struct WeatherView: View {
                 ProgressView()
             }
         }
-        .frame(width:200, height:200)
+        .frame(width:300, height:300)
         .background(.ultraThinMaterial)
         .cornerRadius(20)
         .onAppear{
@@ -47,7 +47,7 @@ struct WeatherView: View {
         
     private func fetchWeatherData(for location: CLLocation){
             let apiKey = "b2e5d7442278a30909dcc92e3ae3f7c9"
-            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&units=imperial&appid=\(apiKey)"
+            let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&units=metric&appid=\(apiKey)"
             print(urlString)
             
             guard let url = URL(string: urlString)else {return}
@@ -78,9 +78,7 @@ struct WeatherView: View {
 }
     
 struct WeatherView_Previews: PreviewProvider {
-    @State static var weatherData: WeatherData? = nil
-    
     static var previews: some View {
-        WeatherView(weatherData: $weatherData)
+        WeatherView()
     }
 }
